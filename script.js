40f72671ab0dbcd710128e92bddd068cef14bb9c
Denis1099 <81426532+Denis1099@users.noreply.github.com>
2024-11-25 22:59:25 +0200

    Add UI and update game logic

--- script.js
+++ script.js
@@ -1,51 +1,91 @@
+/// a function for getting the computers choice in the game
 const getComputerChoice = () => {
     let randomnum = Math.floor(Math.random() * 3);
     if(randomnum === 0){
-        return "Rock";
+        return "rock";
     } else if(randomnum === 1){
-        return "Scissors";
+        return "scissors";
     } else if (randomnum === 2){
-        return "Paper";
+        return "paper";
     }
 };
 
-const getHumanChoice = () => {
-    const sign = prompt("Rock, Paper or Scissors?"); 
-    return sign.toLowerCase();
+const getHumanChoice = (e) => {
+    const sign = e.target.id; 
+    return sign;
 }
-
-const playRound = (humanChoice, computerChoice) => {
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie!");
-        return [0, 0];
-    } else if (humanChoice === "rock" && computerChoice === "Paper") {
-        console.log("You lose! Paper beats Rock.");
-        return [0, 1];
-    } else if (humanChoice === "paper" && computerChoice === "Scissors") {
-        console.log("You lose! Scissors beats Paper.");
-        return [0, 1];
-    } else if (humanChoice === "scissors" && computerChoice === "Rock") {
-        console.log("You lose! Rock beats Scissors.");
-        return [0, 1];
-    } else {
-        console.log("You win!");
-        return [1, 0];
-    }
-};
-
-function playGame(){
     let humanScore = 0;
-    let computerScore = 0;
-    for(let i=0; i<5;i++){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        let [humanScore1, computerScore1] = playRound(humanChoice, computerChoice);
-        humanScore += humanScore1;
-        computerScore += computerScore1;
-        
-    }
-    return [computerScore, humanScore];
-}
+    let robotScore = 0;
+/// a function which decides who wins in the round
+const playRound = (humanChoice, computerChoice) => {
+    const roundResults = document.querySelector('#roundResults');
+    let humanScoreDisplay = document.querySelector('#humanScoreDisplay');
+    let robotScoreDisplay = document.querySelector('#robotScoreDisplay');
 
-const result = playGame();
-console.log("Final scores:", result);
+      roundResults.textContent = "";
+      if (humanChoice === computerChoice) {
+          roundResults.textContent = "It's a tie!";
+      } else if (humanChoice === "rock" && computerChoice === "paper") {
+          roundResults.textContent = "You lose! Paper beats Rock.";
+          robotScore += 1;
+      } else if (humanChoice === "paper" && computerChoice === "scissors") {
+          roundResults.textContent = "You lose! Scissors beats Paper.";
+          robotScore += 1;
+      } else if (humanChoice === "scissors" && computerChoice === "rock") {
+          roundResults.textContent = "You lose! Rock beats Scissors.";
+          robotScore += 1;
+      } else {
+          roundResults.textContent = "You win!";
+          humanScore += 1;
+      }
+      if(humanScore === 5){
+        alert("You won!");
+        humanScore = 0;
+        robotScore = 0;
+      } else if(robotScore === 5){
+        alert("You lost, try again");
+        humanScore = 0;
+        robotScore = 0;
+      }
+      humanScoreDisplay.textContent ="Your score:" +humanScore;
+      robotScoreDisplay.textContent ="Opponent's score:" +robotScore;
+      console.log(humanScore);
+      console.log(robotScore);
+};
+
+const choiceBtn = document.querySelectorAll("button");
+
+choiceBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const humanChoice = getHumanChoice(e);
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    })
+});
+
+// goal: display the current score for human and robot, update the points each round
+// 1. set both scores to 0, whenever someone wins a round add a point
+// 
+// 
+// 
+// 
+// 
+
+/// a rock, paper, scissors game to five rounds
+// function playGame(){
+//     let humanScore = 0;
+//     let computerScore = 0;
+//     for(let i=0; i<5;i++){
+//         let humanChoice = getHumanChoice();
+//         let computerChoice = getComputerChoice();
+//         let [humanScore1, computerScore1] = playRound(humanChoice, computerChoice);
+//         humanScore += humanScore1;
+//         computerScore += computerScore1;
+        
+//     }
+//     return [computerScore, humanScore];
+// }
+
+// const result = playGame();
+// console.log("Final scores:", result);
+
